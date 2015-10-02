bc7f676236a9f0a7148e64fc84a8b4b0f7def005
leocherubim <leo.fcherubini@gmail.com>
2015-10-02 09:28:31 +0000

    Configuracao das rotas dos CRUDs das Models Category e Product

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,3 +20,90 @@ Route::get('exemplo', 'WelcomeController@exemplo');
 Route::get('admin/categories', 'AdminCategoriesController@index');
 
 Route::get('admin/categories', 'AdminProductsController@index');
+
+/*
+ * Grupo de rotas para o prefixo admin
+ */
+Route::group(['prefix'=>'admin', function() {
+
+	/*
+	 * Rota de visualizacao de todas Categories
+	 */
+	Route::get(
+		'categories',
+		['as'=>'categories', 'uses'=>'AdminCategoriesController@index']
+	);
+
+	/*
+	 * Rota de visualizacao da Category do parametro id
+	 */
+	Route::get(
+		'categories/{id}',
+		['as'=>'categories.show', 'uses'=>'AdminCategoriesController@show']
+	);
+
+	/*
+	 * Rota de insersao da Category
+	 */
+	Route::post(
+		'categories',
+		['as'=>'categories.store', 'uses'=>'AdminCategoriesController@store']
+	);
+
+	/*
+	 * Rota de atualizacao da Category
+	 */
+	Route::put(
+		'categories/{id}',
+		['as'=>'categories.update', 'uses'=>'AdminCategoriesController@update']
+	);
+
+	/*
+	 * Rota de remocao da Category
+	 */
+	Route::get(
+		'categories/remove/{id}',
+		['as'=>'categories.update', 'uses'=>'AdminCategoriesController@update']
+	);
+
+	/*
+	 * Rota de visualizacao de todas Products
+	 */
+	Route::get(
+		'products',
+		['as'=>'products', 'uses'=>'AdminProductsController@index']
+	);
+
+	/*
+	 * Rota de visualizacao da Product do parametro id
+	 */
+	Route::get(
+		'products/{id}',
+		['as'=>'products.show', 'uses'=>'AdminProductsController@show']
+	);
+
+	/*
+	 * Rota de insersao da Product
+	 */
+	Route::post(
+		'products',
+		['as'=>'products.store', 'uses'=>'AdminProductsController@store']
+	);
+
+	/*
+	 * Rota de atualizacao da Product
+	 */
+	Route::put(
+		'products/{id}',
+		['as'=>'products.update', 'uses'=>'AdminProductsController@update']
+	);
+
+	/*
+	 * Rota de remocao da Product
+	 */
+	Route::get(
+		'products/remove/{id}',
+		['as'=>'products.update', 'uses'=>'AdminProductsController@update']
+	);
+
+}]);
